697f2c8c18c817fea0e9da5742ca61876f8f7571
Alexandr Brezin <sasha.brezin7262@gmail.com>
2020-05-22 19:14:30 +0300

    Была создана форма и подкоючина DataGrid и еще по мелочи

--- UpdateDB.cs
+++ UpdateDB.cs
@@ -7,14 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Entity;
 
 namespace TestPlanetsNum3
 {
     public partial class UpdateDB : Form
     {
+        PlanetsContext db;
         public UpdateDB()
         {
             InitializeComponent();
+            db = new PlanetsContext();
+            db.Planets.Load();
+            dataGridView1.DataSource = db.Planets.Local.ToBindingList();
         }
     }
 }
